2ec50535b5735e64a62b695079bd79396556324e
Matthew Brozen <matthewbrozen@Matthews-MacBook-Pro-2.local>
2023-05-14 16:28:26 -0700

    ready to review

--- privacy.js
+++ privacy.js
@@ -72,6 +72,178 @@ document.querySelector("#dataOut").addEventListener('click', function(){
 
 })
 
+
+document.querySelector("#collectIn").addEventListener('click', function(){
+    document.getElementById("collectText").style.display = "block";
+    document.getElementById("collectIn").style.display = "none";
+    document.getElementById("collectOut").style.display = "block";
+    document.getElementById("collectRow").style.border='none';
+    
+
+})
+
+document.querySelector("#collectOut").addEventListener('click', function(){
+    document.getElementById("collectText").style.display = "none";
+    document.getElementById("collectIn").style.display = "block";
+    document.getElementById("collectOut").style.display = "none";
+    document.getElementById("collectRow").style.borderBottom='1px solid white';
+
+})
+
+
+document.querySelector("#shareIn").addEventListener('click', function(){
+    document.getElementById("shareText").style.display = "block";
+    document.getElementById("shareIn").style.display = "none";
+    document.getElementById("shareOut").style.display = "block";
+    document.getElementById("shareRow").style.border='none';
+    
+
+})
+
+document.querySelector("#shareOut").addEventListener('click', function(){
+    document.getElementById("shareText").style.display = "none";
+    document.getElementById("shareIn").style.display = "block";
+    document.getElementById("shareOut").style.display = "none";
+    document.getElementById("shareRow").style.borderBottom='1px solid white';
+
+})
+
+document.querySelector("#choiceIn").addEventListener('click', function(){
+    document.getElementById("choiceText").style.display = "block";
+    document.getElementById("choiceIn").style.display = "none";
+    document.getElementById("choiceOut").style.display = "block";
+    document.getElementById("choiceRow").style.border='none';
+    
+
+})
+
+document.querySelector("#choiceOut").addEventListener('click', function(){
+    document.getElementById("choiceText").style.display = "none";
+    document.getElementById("choiceIn").style.display = "block";
+    document.getElementById("choiceOut").style.display = "none";
+    document.getElementById("choiceRow").style.borderBottom='1px solid white';
+
+})
+
+document.querySelector("#personIn").addEventListener('click', function(){
+    document.getElementById("personText").style.display = "block";
+    document.getElementById("personIn").style.display = "none";
+    document.getElementById("personOut").style.display = "block";
+    document.getElementById("personRow").style.border='none';
+    
+
+})
+
+document.querySelector("#personOut").addEventListener('click', function(){
+    document.getElementById("personText").style.display = "none";
+    document.getElementById("personIn").style.display = "block";
+    document.getElementById("personOut").style.display = "none";
+    document.getElementById("personRow").style.borderBottom='1px solid white';
+
+})
+
+document.querySelector("#busIn").addEventListener('click', function(){
+    document.getElementById("busText").style.display = "block";
+    document.getElementById("busIn").style.display = "none";
+    document.getElementById("busOut").style.display = "block";
+    document.getElementById("busRow").style.border='none';
+    
+
+})
+
+document.querySelector("#busOut").addEventListener('click', function(){
+    document.getElementById("busText").style.display = "none";
+    document.getElementById("busIn").style.display = "block";
+    document.getElementById("busOut").style.display = "none";
+    document.getElementById("busRow").style.borderBottom='1px solid white';
+
+})
+
+document.querySelector("#childIn").addEventListener('click', function(){
+    document.getElementById("childText").style.display = "block";
+    document.getElementById("childIn").style.display = "none";
+    document.getElementById("childOut").style.display = "block";
+    document.getElementById("childRow").style.border='none';
+    
+
+})
+
+document.querySelector("#childOut").addEventListener('click', function(){
+    document.getElementById("childText").style.display = "none";
+    document.getElementById("childIn").style.display = "block";
+    document.getElementById("childOut").style.display = "none";
+    document.getElementById("childRow").style.borderBottom='1px solid white';
+
+})
+
+document.querySelector("#euIn").addEventListener('click', function(){
+    document.getElementById("euText").style.display = "block";
+    document.getElementById("euIn").style.display = "none";
+    document.getElementById("euOut").style.display = "block";
+    document.getElementById("euRow").style.border='none';
+    
+
+})
+
+document.querySelector("#euOut").addEventListener('click', function(){
+    document.getElementById("euText").style.display = "none";
+    document.getElementById("euIn").style.display = "block";
+    document.getElementById("euOut").style.display = "none";
+    document.getElementById("euRow").style.borderBottom='1px solid white';
+
+})
+
+document.querySelector("#caIn").addEventListener('click', function(){
+    document.getElementById("caText").style.display = "block";
+    document.getElementById("caIn").style.display = "none";
+    document.getElementById("caOut").style.display = "block";
+    document.getElementById("caRow").style.border='none';
+    
+
+})
+
+document.querySelector("#caOut").addEventListener('click', function(){
+    document.getElementById("caText").style.display = "none";
+    document.getElementById("caIn").style.display = "block";
+    document.getElementById("caOut").style.display = "none";
+    document.getElementById("caRow").style.borderBottom='1px solid white';
+
+})
+
+document.querySelector("#changeIn").addEventListener('click', function(){
+    document.getElementById("changeText").style.display = "block";
+    document.getElementById("changeIn").style.display = "none";
+    document.getElementById("changeOut").style.display = "block";
+    document.getElementById("changeRow").style.border='none';
+    
+
+})
+
+document.querySelector("#changeOut").addEventListener('click', function(){
+    document.getElementById("changeText").style.display = "none";
+    document.getElementById("changeIn").style.display = "block";
+    document.getElementById("changeOut").style.display = "none";
+    document.getElementById("changeRow").style.borderBottom='1px solid white';
+
+})
+
+document.querySelector("#forIn").addEventListener('click', function(){
+    document.getElementById("forText").style.display = "block";
+    document.getElementById("forIn").style.display = "none";
+    document.getElementById("forOut").style.display = "block";
+    document.getElementById("forRow").style.border='none';
+    
+
+})
+
+document.querySelector("#forOut").addEventListener('click', function(){
+    document.getElementById("forText").style.display = "none";
+    document.getElementById("forIn").style.display = "block";
+    document.getElementById("forOut").style.display = "none";
+    document.getElementById("forRow").style.borderBottom='1px solid white';
+
+})
+
 document.querySelector("#aboutUs").addEventListener('click', function(){    
     console.log('test')
 });
