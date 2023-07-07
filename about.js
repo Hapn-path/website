6d017fbdc60b115ac00d3034e63c01f4164faef1
Matthew Brozen <matthewbrozen@Matthews-MacBook-Pro-2.local>
2023-07-07 16:46:56 -0700

    top bar and email

--- about.js
+++ about.js
@@ -48,3 +48,12 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+  document.querySelector("#return").addEventListener('click', function(){    
+    window.location.href="mobile_template.html"
+});
+
+document.querySelector("#stop").addEventListener('click', function(){    
+    console.log('stop');
+    event.stopPropagation();
+});
--- antman.js
+++ antman.js
@@ -41,3 +41,12 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+
+  document.querySelector("#return").addEventListener('click', function(){    
+    window.location.href="mobile_template.html"
+});
+
+document.querySelector("#zDex").addEventListener('click', function(){    
+    event.stopPropagation();
+});
